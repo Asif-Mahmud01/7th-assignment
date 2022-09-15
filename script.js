3cc39b5d377970b45855b0af7f782a258084430a
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-09-15 17:06:30 +0600

    nineth commit

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ let startTime;
 let questionText = "";
 
 // Load and display question
-fetch("./texts.json")
+fetch("texts.json")
   .then((res) => res.json())
   .then((data) => {
+    console.log(data.length);
     questionText = data[Math.floor(Math.random() * data.length)];
     question.innerHTML = questionText;
   });
@@ -46,6 +47,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -60,80 +62,3 @@ const validate = (key) => {
   }
   return false;
 };
-
-// FINISHED TYPING
-const gameOver = () => {
-  document.removeEventListener("keydown", typeController);
-  // the current time is the finish time
-  // so total time taken is current time - start time
-  const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
-
-  // show result modal
-  resultModal.innerHTML = "";
-  resultModal.classList.toggle("hidden");
-  modalBackground.classList.toggle("hidden");
-  // clear user text
-  display.innerHTML = "";
-  // make it inactive
-  display.classList.add("inactive");
-  // show result
-  resultModal.innerHTML += `
-    <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
-  `;
-
-  addHistory(questionText, timeTaken, errorCount);
-
-  // restart everything
-  startTime = null;
-  errorCount = 0;
-  userText = "";
-  display.classList.add("inactive");
-};
-
-const closeModal = () => {
-  modalBackground.classList.toggle("hidden");
-  resultModal.classList.toggle("hidden");
-};
-
-const start = () => {
-  // If already started, do not start again
-  if (startTime) return;
-
-  let count = 3;
-  countdownOverlay.style.display = "flex";
-
-  const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
-
-    // finished timer
-    if (count == 0) {
-      // -------------- START TYPING -----------------
-      document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
-      display.classList.remove("inactive");
-
-      clearInterval(startCountdown);
-      startTime = new Date().getTime();
-    }
-    count--;
-  }, 1000);
-};
-
-// START Countdown
-startBtn.addEventListener("click", start);
-
-// If history exists, show it
-displayHistory();
-
-// Show typing time spent
-setInterval(() => {
-  const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
-
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
-}, 1000);
