54f594a25b1be3fc60292135a330849123c67269
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-09-15 17:04:17 +0600

    seventh commit

--- history.js
+++ history.js
@@ -20,21 +20,3 @@ function addHistory(questionText, timeTaken, errorCount) {
 
   displayHistory();
 }
-
-function displayHistory() {
-  histories.innerHTML = "";
-  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-
-  previousTests.forEach((test) => {
-    const newRow = document.createElement("div");
-    newRow.classList.add("card");
-
-    newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
-  `;
-
-    histories.appendChild(newRow);
-  });
-}
